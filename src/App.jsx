ef87c9ec5c1db3538f6bbbae03aa542ba79e0b8c
Jeffrey <Jefferymsd101@gmail.com>
2026-06-11 15:34:03 +0530

    Add expense creation and display

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,57 @@ import { useState } from "react"
 
 function App() {
 
-  const [expenses, setExpenses] = useState([])
+ const [expenses, setExpenses] = useState([])
+ const [name, setName] = useState("")
+ const [amount, setAmount] = useState("")
+ const [category, setCategory] = useState("")
+ const [date, setDate] = useState("")
+
+ function handleAddExpenses(){
+    if (name === "" || amount === "" || category === "" || date === ""){
+      alert("Please fill all fields")
+      return
+    }
+    const newExpense = {
+    id: Date.now(),
+    name,
+    amount,
+    category,
+    date
+    }
+    setExpenses([...expenses,newExpense])
+
+    setName("")
+    setAmount("")
+    setCategory("")
+    setDate("")
+
+  }
 
   return(
-    <div>
-      <h1>Expense Tracker</h1>
-    </div>
+   <>
+      <div>
+        <h1>Expense Tracker</h1>
+      </div>
+      <div>
+        <input type="text" placeholder="Expense Name" value={name} onChange={(event)=> setName(event.target.value)} />
+        <input type="number" placeholder="Amount" value={amount} onChange={(event)=> setAmount(event.target.value)} />
+        <input type="text" placeholder="Category" value={category} onChange={(event)=> setCategory(event.target.value)} />
+        <input type="date" value={date} onChange={(event)=> setDate(event.target.value)} />
+        <button onClick={handleAddExpenses}>Add Expense</button>
+      </div>
+      <h2>Total Expenses: {expenses.length}</h2>
+      
+     {expenses.map((expense) => (
+       <div key={expense.id}>
+         <p>{expense.name}</p>
+         <p>{expense.amount}</p>
+         <p>{expense.category}</p>
+         <p>{expense.date}</p>
+         <button>Delete</button>
+        </div>
+      ))}
+   </>
   )
 }
 export default App
