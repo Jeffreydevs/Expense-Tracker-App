28cf220fea050045e5121ffece91f3e35e34711a
Jeffrey <Jefferymsd101@gmail.com>
2026-06-11 11:19:42 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+function App() {
+
+  const [expenses, setExpenses] = useState([])
+
+  return(
+    <div>
+      <h1>Expense Tracker</h1>
+    </div>
+  )
+}
+export default App
